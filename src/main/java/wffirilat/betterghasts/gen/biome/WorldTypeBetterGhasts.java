375637e9968b692f8f06a71cda82e8775ec0c3ad
wffirilat <william.eqr@gmail.com>
2015-04-05 00:21:23 -0500

    Prepared to add biomes

--- src/main/java/wffirilat/betterghasts/gen/biome/WorldTypeBetterGhasts.java
+++ src/main/java/wffirilat/betterghasts/gen/biome/WorldTypeBetterGhasts.java
@@ -1,11 +1,21 @@
 package wffirilat.betterghasts.gen.biome;
 
 import net.minecraft.world.WorldType;
+import net.minecraft.world.gen.layer.GenLayer;
+import net.minecraft.world.gen.layer.GenLayerBiomeEdge;
+import net.minecraft.world.gen.layer.GenLayerZoom;
 
 public class WorldTypeBetterGhasts extends WorldType{
 
 	public WorldTypeBetterGhasts(String name) {
 		super(name);
+	}
+	
+	public GenLayer getBiomeLayer(long worldSeed, GenLayer parentLayer) {
+		GenLayer ret = new BetterGhastsLayer(200L, parentLayer, this);
+		ret = GenLayerZoom.magnify(1000L, ret, 2);
+		ret = new GenLayerBiomeEdge(1000L, ret);
+		return ret;
 	}
 
 }
