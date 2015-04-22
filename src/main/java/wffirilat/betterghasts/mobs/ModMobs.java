b3c18d2a62675cbfe1b354741137662f0ad1c207
wffirilat <william.eqr@gmail.com>
2015-04-22 10:28:01 -0500

    Revert "added base for PoisonGhast"
    This reverts commit b1239a5e7fb867027ca09b73299db8522c900c24.

--- src/main/java/wffirilat/betterghasts/mobs/ModMobs.java
+++ src/main/java/wffirilat/betterghasts/mobs/ModMobs.java
@@ -9,7 +9,6 @@ public class ModMobs {
 	public static void init() {
 		
 		createEntity(FlameGhast.class, "flameGhast", 0x150a07, 0x4f271a);
-		createEntity(PoisonGhast.class, "poisonGhast", 0x150a07, 0x4f271a);
 
 		//TODO 1- creepy ghast (More explosive)
 		//TODO 1- blaze ghast
--- src/main/java/wffirilat/betterghasts/proxy/ClientProxy.java
+++ src/main/java/wffirilat/betterghasts/proxy/ClientProxy.java
@@ -1,18 +1,15 @@
 package wffirilat.betterghasts.proxy;
 
-import wffirilat.betterghasts.mobs.FlameGhast;
-import wffirilat.betterghasts.mobs.PoisonGhast;
-import wffirilat.betterghasts.mobs.RenderFlameGhast;
-import wffirilat.betterghasts.mobs.RenderPoisonGhast;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import wffirilat.betterghasts.mobs.FlameGhast;
+import wffirilat.betterghasts.mobs.RenderFlameGhast;
 
 public class ClientProxy extends CommonProxy{
 	
 	public void registerRenderers() {
 		
 		RenderingRegistry.registerEntityRenderingHandler(FlameGhast.class, new RenderFlameGhast());
-		RenderingRegistry.registerEntityRenderingHandler(PoisonGhast.class, new RenderPoisonGhast());
-
+		
 	}
 
 }
