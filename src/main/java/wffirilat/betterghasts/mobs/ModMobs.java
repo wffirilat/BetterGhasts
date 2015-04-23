9276708babf30a0df02498939f445df4348ad3c8
Kindler <Mjhock99@gmail.com>
2015-04-22 20:33:28 -0500

    Blind ghast on fireball hit. working on ai target lock. Not main branch, just for view

--- src/main/java/wffirilat/betterghasts/mobs/ModMobs.java
+++ src/main/java/wffirilat/betterghasts/mobs/ModMobs.java
@@ -10,6 +10,7 @@ public class ModMobs {
 		
 		createEntity(FlameGhast.class, "flameGhast", 0x150a07, 0x4f271a);
 		createEntity(PoisonGhast.class, "poisonGhast", 0x150a07, 0x4f271a);
+		createEntity(BlindGhast.class, "blindGhast", 0x150a07, 0x4f271a);
 
 		//TODO 1- creepy ghast (More explosive)
 		//TODO 1- blaze ghast
