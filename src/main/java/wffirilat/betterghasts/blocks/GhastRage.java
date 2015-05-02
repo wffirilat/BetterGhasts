4aa79b0bc23c525c4ef4b5a3e6aa171a67cfe5c5
wffirilat <william.eqr@gmail.com>
2015-05-02 12:43:49 -0500

    Deobfuscated all the code to promote actual understanding. Read it, ant understand what's going on.

--- src/main/java/wffirilat/betterghasts/blocks/GhastRage.java
+++ src/main/java/wffirilat/betterghasts/blocks/GhastRage.java
@@ -26,9 +26,9 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class GhastRage extends Block
 {
     @SideOnly(Side.CLIENT)
-    private IIcon field_150116_a;
+    private IIcon topTexture;
     @SideOnly(Side.CLIENT)
-    private IIcon field_150115_b;
+    private IIcon bottomTexture;
     private static final String __OBFID = "CL_00000324";
 
     public GhastRage(String name)
@@ -44,22 +44,22 @@ public class GhastRage extends Block
      * Gets the block's texture. Args: side, meta
      */
     @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int p_149691_1_, int p_149691_2_)
+    public IIcon getIcon(int side, int meta)
     {
-        return p_149691_1_ == 0 ? this.field_150115_b : (p_149691_1_ == 1 ? this.field_150116_a : this.blockIcon);
+        return side == 0 ? this.bottomTexture : (side == 1 ? this.topTexture : this.blockIcon);
     }
 
     /**
      * Called whenever the block is added into the world. Args: world, x, y, z
      */
-    public void onBlockAdded(World p_149726_1_, int p_149726_2_, int p_149726_3_, int p_149726_4_)
+    public void onBlockAdded(World world, int x, int y, int z)
     {
-        super.onBlockAdded(p_149726_1_, p_149726_2_, p_149726_3_, p_149726_4_);
+        super.onBlockAdded(world, x, y, z);
 
-        if (p_149726_1_.isBlockIndirectlyGettingPowered(p_149726_2_, p_149726_3_, p_149726_4_))
+        if (world.isBlockIndirectlyGettingPowered(x, y, z))
         {
-            this.onBlockDestroyedByPlayer(p_149726_1_, p_149726_2_, p_149726_3_, p_149726_4_, 1);
-            p_149726_1_.setBlockToAir(p_149726_2_, p_149726_3_, p_149726_4_);
+            this.onBlockDestroyedByPlayer(world, x, y, z, 1);
+            world.setBlockToAir(x, y, z);
         }
     }
 
@@ -67,19 +67,19 @@ public class GhastRage extends Block
      * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
      * their own) Args: x, y, z, neighbor Block
      */
-    public void onNeighborBlockChange(World p_149695_1_, int p_149695_2_, int p_149695_3_, int p_149695_4_, Block p_149695_5_)
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block neighbor)
     {
-        if (p_149695_1_.isBlockIndirectlyGettingPowered(p_149695_2_, p_149695_3_, p_149695_4_))
+        if (world.isBlockIndirectlyGettingPowered(x, y, z))
         {
-            this.onBlockDestroyedByPlayer(p_149695_1_, p_149695_2_, p_149695_3_, p_149695_4_, 1);
-            p_149695_1_.setBlockToAir(p_149695_2_, p_149695_3_, p_149695_4_);
+            this.onBlockDestroyedByPlayer(world, x, y, z, 1);
+            world.setBlockToAir(x, y, z);
         }
     }
 
     /**
      * Returns the quantity of items to drop on block destruction.
      */
-    public int quantityDropped(Random p_149745_1_)
+    public int quantityDropped(Random rand)
     {
         return 1;
     }
@@ -87,33 +87,33 @@ public class GhastRage extends Block
     /**
      * Called upon the block being destroyed by an explosion
      */
-    public void onBlockDestroyedByExplosion(World p_149723_1_, int p_149723_2_, int p_149723_3_, int p_149723_4_, Explosion p_149723_5_)
+    public void onBlockDestroyedByExplosion(World world, int x, int y, int z, Explosion explosion)
     {
-        if (!p_149723_1_.isRemote)
+        if (!world.isRemote)
         {
-            PrimedGhastRage PrimedGhastRage = new PrimedGhastRage(p_149723_1_, (double)((float)p_149723_2_ + 0.5F), (double)((float)p_149723_3_ + 0.5F), (double)((float)p_149723_4_ + 0.5F), p_149723_5_.getExplosivePlacedBy());
-            PrimedGhastRage.fuse = p_149723_1_.rand.nextInt(PrimedGhastRage.fuse / 4) + PrimedGhastRage.fuse / 8;
-            p_149723_1_.spawnEntityInWorld(PrimedGhastRage);
+            PrimedGhastRage PrimedGhastRage = new PrimedGhastRage(world, (double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), explosion.getExplosivePlacedBy());
+            PrimedGhastRage.fuse = world.rand.nextInt(PrimedGhastRage.fuse / 4) + PrimedGhastRage.fuse / 8;
+            world.spawnEntityInWorld(PrimedGhastRage);
         }
     }
 
     /**
      * Called right before the block is destroyed by a player.  Args: world, x, y, z, metaData
      */
-    public void onBlockDestroyedByPlayer(World p_149664_1_, int p_149664_2_, int p_149664_3_, int p_149664_4_, int p_149664_5_)
+    public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int meta)
     {
-        this.func_150114_a(p_149664_1_, p_149664_2_, p_149664_3_, p_149664_4_, p_149664_5_, (EntityLivingBase)null);
+        this.explode(world, x, y, z, meta, (EntityLivingBase)null);
     }
 
-    public void func_150114_a(World p_150114_1_, int p_150114_2_, int p_150114_3_, int p_150114_4_, int p_150114_5_, EntityLivingBase p_150114_6_)
+    public void explode(World world, int x, int y, int z, int doExplode, EntityLivingBase entity)
     {
-        if (!p_150114_1_.isRemote)
+        if (!world.isRemote)
         {
-            if ((p_150114_5_ & 1) == 1)
+            if ((doExplode & 1) == 1) // is doExplode right?
             {
-                PrimedGhastRage PrimedGhastRage = new PrimedGhastRage(p_150114_1_, (double)((float)p_150114_2_ + 0.5F), (double)((float)p_150114_3_ + 0.5F), (double)((float)p_150114_4_ + 0.5F), p_150114_6_);
-                p_150114_1_.spawnEntityInWorld(PrimedGhastRage);
-                p_150114_1_.playSoundAtEntity(PrimedGhastRage, "game.tnt.primed", 1.0F, 1.0F);
+                PrimedGhastRage PrimedGhastRage = new PrimedGhastRage(world, (double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), entity);
+                world.spawnEntityInWorld(PrimedGhastRage);
+                world.playSoundAtEntity(PrimedGhastRage, "game.tnt.primed", 1.0F, 1.0F);
             }
         }
     }
@@ -121,34 +121,34 @@ public class GhastRage extends Block
     /**
      * Called upon block activation (right click on the block.)
      */
-    public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer p_149727_5_, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float sideX, float sideY, float sideZ)
     {
-        if (p_149727_5_.getCurrentEquippedItem() != null && p_149727_5_.getCurrentEquippedItem().getItem() == Items.flint_and_steel)
+        if (player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() == Items.flint_and_steel)
         {
-            this.func_150114_a(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_, 1, p_149727_5_);
-            p_149727_1_.setBlockToAir(p_149727_2_, p_149727_3_, p_149727_4_);
-            p_149727_5_.getCurrentEquippedItem().damageItem(1, p_149727_5_);
+            this.explode(world, x, y, z, 1, player);
+            world.setBlockToAir(x, y, z);
+            player.getCurrentEquippedItem().damageItem(1, player);
             return true;
         }
         else
         {
-            return super.onBlockActivated(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_, p_149727_5_, p_149727_6_, p_149727_7_, p_149727_8_, p_149727_9_);
+            return super.onBlockActivated(world, x, y, z, player, side, sideX, sideY, sideZ);
         }
     }
 
     /**
      * Triggered whenever an entity collides with this block (enters into the block). Args: world, x, y, z, entity
      */
-    public void onEntityCollidedWithBlock(World p_149670_1_, int p_149670_2_, int p_149670_3_, int p_149670_4_, Entity p_149670_5_)
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
     {
-        if (p_149670_5_ instanceof EntityArrow && !p_149670_1_.isRemote)
+        if (entity instanceof EntityArrow && !world.isRemote)
         {
-            EntityArrow entityarrow = (EntityArrow)p_149670_5_;
+            EntityArrow entityarrow = (EntityArrow)entity;
 
             if (entityarrow.isBurning())
             {
-                this.func_150114_a(p_149670_1_, p_149670_2_, p_149670_3_, p_149670_4_, 1, entityarrow.shootingEntity instanceof EntityLivingBase ? (EntityLivingBase)entityarrow.shootingEntity : null);
-                p_149670_1_.setBlockToAir(p_149670_2_, p_149670_3_, p_149670_4_);
+                this.explode(world, x, y, z, 1, entityarrow.shootingEntity instanceof EntityLivingBase ? (EntityLivingBase)entityarrow.shootingEntity : null);
+                world.setBlockToAir(x, y, z);
             }
         }
     }
@@ -156,16 +156,16 @@ public class GhastRage extends Block
     /**
      * Return whether this block can drop from an explosion.
      */
-    public boolean canDropFromExplosion(Explosion p_149659_1_)
+    public boolean canDropFromExplosion(Explosion explosion)
     {
         return false;
     }
 
     @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister p_149651_1_)
+    public void registerBlockIcons(IIconRegister registry)
     {
-        this.blockIcon = p_149651_1_.registerIcon(this.getTextureName() + "_side");
-        this.field_150116_a = p_149651_1_.registerIcon(this.getTextureName() + "_top");
-        this.field_150115_b = p_149651_1_.registerIcon(this.getTextureName() + "_bottom");
+        this.blockIcon = registry.registerIcon(this.getTextureName() + "_side");
+        this.topTexture = registry.registerIcon(this.getTextureName() + "_top");
+        this.bottomTexture = registry.registerIcon(this.getTextureName() + "_bottom");
     }
 }
