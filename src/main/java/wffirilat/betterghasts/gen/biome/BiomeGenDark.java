fe43901be8ed17d763d2b7659943c16cf94edd64
wffirilat <william.eqr@gmail.com>
2015-05-15 19:41:48 -0500

    Arcane ghast texture

--- src/main/java/wffirilat/betterghasts/gen/biome/BiomeGenDark.java
+++ src/main/java/wffirilat/betterghasts/gen/biome/BiomeGenDark.java
@@ -12,13 +12,14 @@ public class BiomeGenDark extends BiomeGenJungle {
 
 	public BiomeGenDark(int p_i1971_1_) {
 		super(p_i1971_1_, true);
+		this.waterColorMultiplier = 0;
 		this.spawnableMonsterList.add(new SpawnListEntry(VoidGhast.class, 40, 1, 1));
 		this.spawnableMonsterList.add(new SpawnListEntry(BlindGhast.class, 40, 1, 1));
-		this.theBiomeDecorator = new BiomeDecoratorPoison();
 		this.topBlock = ModBlocks.shadow;
 		this.fillerBlock = ModBlocks.shadow;
-		this.theBiomeDecorator.treesPerChunk = 10;
-		this.theBiomeDecorator.grassPerChunk = 25;
-		this.theBiomeDecorator.flowersPerChunk = 4;
+	}
+	
+	public int getSkyColorByTemp(float temp) {
+		return 0;
 	}
 }
